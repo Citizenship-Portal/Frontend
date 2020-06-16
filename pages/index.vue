de7fbb76ef4d7c46e670c9f18700896a502b7aae
ricky <rickylalwani@live.com>
2020-06-15 20:15:19 -0700

    Store result of disclaimer

--- pages/index.vue
+++ pages/index.vue
@@ -18,7 +18,7 @@
             <button class="usa-button">Create Account
             </button>
           </nuxt-link>
-          <nuxt-link to="/login">
+          <nuxt-link to="/signin">
             <button class="usa-button">Current
               Account
             </button>
@@ -40,20 +40,21 @@
     <Footer/>
 
 
-    <Modal :open="open" @close="open = false">
+    <Modal :open="open" @close="accept">
       <div class="tw-max-w-xl tw-w-full tw-bg-white tw-rounded-lg tw-shadow-2xl tw-px-6 tw-py-6 tw-mx-auto">
         <h2 class="font-semibold text-gray-90 tw-text-4xl tw-leading-tight tw-border-b-2 tw-border-gray-20 tw-pb-4">
           Please Read</h2>
-        <p class="tw-mt-4 tw-text-gray-70 tw-text-xl tw-leading-relaxed">This is NOT an official site of the US Government in any way. This is a demo
+        <p class="tw-mt-4 tw-text-gray-70 tw-text-xl tw-leading-relaxed">This is NOT an official site of the US
+          Government in any way. This is a demo
           concept of an idea <a href="https://twitter.com/andrewyang/status/1262012732402028547">proposed by Andrew
             Yang</a>.</p>
         <div class="tw-mt-6">
           <a href="https://google.com"
-                  class="tw-px-4 visited:tw-text-white tw-py-2 tw-text-white tw-font-semibold tw-bg-red-50 hover:tw-bg-red-60 tw-border tw-border-transparent tw-rounded focus:tw-outline-none focus:tw-shadow-outline">
+             class="tw-inline-block tw-px-4 visited:tw-text-white tw-py-2 tw-text-white tw-font-semibold tw-bg-red-50 hover:tw-bg-red-60 tw-border tw-border-transparent tw-rounded focus:tw-outline-none focus:tw-shadow-outline">
             Leave website
           </a>
-          <button @click="open = false"
-                  class="tw-ml-4 tw-px-4 tw-py-2 tw-text-gray-80 tw-font-semibold tw-bg-white hover:tw-bg-gray-10 tw-border tw-rounded focus:tw-outline-none focus:tw-shadow-outline">
+          <button @click="accept"
+                  class="tw-inline-block tw-mt-4 lg:tw-mt-0 lg:tw-ml-4 tw-px-4 tw-py-2 tw-text-gray-80 tw-font-semibold tw-bg-white hover:tw-bg-gray-10 tw-border tw-rounded focus:tw-outline-none focus:tw-shadow-outline">
             Understood, continue
           </button>
         </div>
@@ -71,11 +72,16 @@ import Modal from "../components/Modal";
 
 export default {
   components: {Modal, Footer, Header},
-  data() {
-    return {
-      open: true
+  computed: {
+    open() {
+      return !this.$store.state.acceptedDisclaimer
     }
   },
+  methods: {
+    accept() {
+      this.$store.commit('accept')
+    }
+  }
 }
 </script>
 
