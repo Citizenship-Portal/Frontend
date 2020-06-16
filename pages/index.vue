969e78d773afe0da7a5f4c6db0f570e19dcd94a5
ricky <rickylalwani@live.com>
2020-06-15 19:54:03 -0700

    Add disclaimer

--- pages/index.vue
+++ pages/index.vue
@@ -38,6 +38,28 @@
       </div>
     </div>
     <Footer/>
+
+
+    <Modal :open="open" @close="open = false">
+      <div class="tw-max-w-xl tw-w-full tw-bg-white tw-rounded-lg tw-shadow-2xl tw-px-6 tw-py-6 tw-mx-auto">
+        <h2 class="font-semibold text-gray-90 tw-text-4xl tw-leading-tight tw-border-b-2 tw-border-gray-20 tw-pb-4">
+          Please Read</h2>
+        <p class="tw-mt-4 tw-text-gray-70 tw-text-xl tw-leading-relaxed">This is NOT an official site of the US Government in any way. This is a demo
+          concept of an idea <a href="https://twitter.com/andrewyang/status/1262012732402028547">proposed by Andrew
+            Yang</a>.</p>
+        <div class="tw-mt-6">
+          <a href="https://google.com"
+                  class="tw-px-4 visited:tw-text-white tw-py-2 tw-text-white tw-font-semibold tw-bg-red-50 hover:tw-bg-red-60 tw-border tw-border-transparent tw-rounded focus:tw-outline-none focus:tw-shadow-outline">
+            Leave website
+          </a>
+          <button @click="open = false"
+                  class="tw-ml-4 tw-px-4 tw-py-2 tw-text-gray-80 tw-font-semibold tw-bg-white hover:tw-bg-gray-10 tw-border tw-rounded focus:tw-outline-none focus:tw-shadow-outline">
+            Understood, continue
+          </button>
+        </div>
+      </div>
+    </Modal>
+
   </div>
 </template>
 
@@ -45,11 +67,15 @@
 
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import Modal from "../components/Modal";
 
 export default {
-  components: {Footer, Header},
-
-
+  components: {Modal, Footer, Header},
+  data() {
+    return {
+      open: true
+    }
+  },
 }
 </script>
 
