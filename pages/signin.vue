9839e4641b97cb2d2413c9257312d4e01705dc8e
ricky <rickylalwani@live.com>
2020-06-16 22:43:04 -0700

    Add logic for sign in

--- pages/signin.vue
+++ pages/signin.vue
@@ -3,16 +3,16 @@
     <Header>Citizenship Portal - Sign In</Header>
 
     <div class="grid-container tw-mt-4 tw-flex-1 tw-w-full tw-flex tw-justify-center" id="main-content">
-      <form class="usa-form usa-form--large tw-w-full">
+      <form class="usa-form usa-form--large tw-w-full" @submit="checkForm">
         <fieldset class="usa-fieldset">
           <legend class="usa-legend">Sign in</legend>
           <span>or <a href="/signup/">create an account</a></span>
 
           <label class="usa-label" for="username">Username or email address</label>
-          <input class="usa-input" id="username" name="username" type="text" autocapitalize="off" autocorrect="off">
+          <input class="usa-input" v-model="username" id="username" name="username" type="text" autocapitalize="off" autocorrect="off">
 
           <label class="usa-label" for="password-sign-in">Password</label>
-          <input class="usa-input" id="password-sign-in" name="password" type="password">
+          <input class="usa-input" v-model="password" id="password-sign-in" name="password" type="password">
           <p class="usa-form__note">
             <a title="Show password" href="javascript:void(0);"
                class="usa-show-password"
@@ -38,7 +38,28 @@ import Footer from "../components/Footer";
 
 export default {
   name: "signin",
-  components: {Footer, Header}
+  components: {Footer, Header},
+  data() {
+    return {
+      errors: [],
+      username: null,
+      password: null
+    }
+  },
+  methods: {
+    checkForm(e) {
+      this.errors = [];
+
+      if (!!this.username && !!this.password) {
+        this.$router.push({path: '/signup/success'})
+      } else {
+        this.errors.push('Please check all fields')
+        console.log(this.errors)
+      }
+
+      e.preventDefault();
+    }
+  }
 }
 </script>
 
