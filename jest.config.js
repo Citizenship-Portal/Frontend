96538abc65986b65f74eb161d77a86c7191abd29
Ahmad Kayyali <kayyali18@gmail.com>
2020-06-24 13:53:05 -0500

    Add jest serializer for snapshots

--- jest.config.js
+++ jest.config.js
@@ -14,5 +14,6 @@ module.exports = {
     '<rootDir>/components/**/*.vue',
     '<rootDir>/pages/**/*.vue'
   ],
+  snapshotSerializers: ['jest-serializer-vue'],
   transformIgnorePatterns: ['<rootDir>/node_modules/']
 }
